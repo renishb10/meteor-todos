9ed6078e5cefa3fe79b486ee12acf570cba082ba
Renish B <renish@truebase.io>
2019-09-02 14:05:37 +0530

    modified app.js

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -7,8 +7,21 @@ import { Tasks } from '../api/tasks.js';
 import Task from './Task.js';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            hideCompleted: true
+        };
+    }
+
     renderTasks() {
-        return this.props.tasks.map(task => (
+        let filteredTasks = this.props.tasks;
+        if (this.state.hideCompleted) {
+            filteredTasks = filteredTasks.filter(task => !task.checked);
+        }
+
+        return filteredTasks.map(task => (
             <Task key={task._id} task={task} />
         ));
     }
@@ -16,19 +29,36 @@ class App extends Component {
     handleSubmit(e) {
         e.preventDefault();
         const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
-        Tasks.insert({
-            text,
-            createdAt: new Date(), // current time
-        });
+        if (text) {
+            Tasks.insert({
+                text,
+                createdAt: new Date(), // current time
+            });
+    
+            ReactDOM.findDOMNode(this.refs.textInput).value = '';
+        }
+    }
 
-        ReactDOM.findDOMNode(this.refs.textInput).value = '';
+    toggleHideCompleted() {
+        this.setState({
+            hideCompleted: !this.state.hideCompleted,
+        });
     }
 
     render() {
         return (
             <div className="container">
                 <header>
-                    <h1>Todo List</h1>
+                    <h1>Todo List ({this.props.incompleteCount})</h1>
+
+                    <label className="hide-completed">
+                        <input type="checkbox"
+                            readOnly
+                            checked={this.state.hideCompleted}
+                            onClick={this.toggleHideCompleted.bind(this)}
+                        />
+                        Hide Completed Tasks
+                    </label>
 
                     <form className="new-task" onSubmit={this.handleSubmit.bind(this)}>
                         <input 
@@ -50,5 +80,6 @@ class App extends Component {
 export default withTracker(() => {
     return {
         tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
+        incompleteCount: Tasks.find({ checked: {$ne: true }}).count(),
     }
 })(App);
